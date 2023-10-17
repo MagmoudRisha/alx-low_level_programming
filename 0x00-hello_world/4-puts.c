654344c26133f1103578aa03812109d81a6dbbca
MagmoudRisha <rishamahmoud09@gmail.com>
2023-10-17 10:32:43 -0700

    prints in the screen using puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 
+/*
+* this program for using puts function instead printf
+*/
+
+
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
